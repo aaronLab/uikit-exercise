91d6fb0646b72bbdf195229c39a86dde9a2995d8
Aaron Lee <aaronlab.net@gmail.com>
2021-04-07 22:23:51 +0900

    user credential

---
+++ SnapKitTest2/SnapKitTest2/Scenes/Main/UserCredential.swift
@@ -0,0 +1,15 @@
+//
+//  UserCredential.swift
+//  SnapKitTest2
+//
+//  Created by Aaron Lee on 2021/04/07.
+//
+
+import Foundation
+
+struct UserCredential {
+    
+    let email: String
+    let password: String
+    
+}
