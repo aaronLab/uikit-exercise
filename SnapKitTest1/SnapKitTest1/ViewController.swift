06af9217d046bec837892c6357368732a2393b8e
Aaron Lee <aaronlab.net@gmail.com>
2021-04-07 14:07:59 +0900

    init proj

---
+++ SnapKitTest1/SnapKitTest1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SnapKitTest1
+//
+//  Created by Aaron Lee on 2021-04-07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
