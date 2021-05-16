c85cbad29448b71f81508bdfcce4a028529a4ddf
Aaron Lee <aaronlab.net@gmail.com>
2021-05-16 13:41:26 +0900

    add sticky image header 2

---
+++ CompleteAutoLayout/CompleteAutoLayout/StickyImageHeader2.swift
@@ -0,0 +1,81 @@
+//
+//  StickyImageHeader2.swift
+//  CompleteAutoLayout
+//
+//  Created by Aaron Lee on 2021/05/05.
+//
+
+import UIKit
+import SnapKit
+
+class StickyImageHeader2: UIViewController, UIScrollViewDelegate {
+    
+    lazy var imageView: UIImageView = {
+        let imageView = UIImageView(image: Shared.Images.portrait3)
+        imageView.clipsToBounds = true
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    private var imageHeight: CGFloat = 300
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Sticky Image Header"
+        view.backgroundColor = .white
+        
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.contentInsetAdjustmentBehavior = .never
+        
+        scrollView.delegate = self
+        
+        view.addSubview(scrollView)
+        scrollView.snp.makeConstraints {
+            $0.leading.equalTo(view.safeAreaLayoutGuide.snp.leading)
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            $0.trailing.equalTo(view.safeAreaLayoutGuide.snp.trailing)
+            $0.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
+        }
+        
+        let contentView = UIView()
+        scrollView.addSubview(contentView)
+        contentView.snp.makeConstraints {
+            $0.leading.equalToSuperview()
+            $0.trailing.equalToSuperview()
+            $0.bottom.equalToSuperview()
+            $0.top.equalToSuperview()
+            $0.width.equalToSuperview()
+            $0.height.equalToSuperview().offset(1)
+        }
+        
+        view.addSubview(imageView)
+        imageView.snp.makeConstraints {
+            $0.leading.equalTo(view.safeAreaLayoutGuide.snp.leading)
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            $0.trailing.equalTo(view.safeAreaLayoutGuide.snp.trailing)
+            $0.height.equalTo(imageHeight)
+        }
+        
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let y = -scrollView.contentOffset.y
+        
+        let height = y >= 0 ? max(imageHeight+y, imageHeight) : min(imageHeight+y, imageHeight)
+        
+        imageView.snp.updateConstraints {
+            $0.height.equalTo(height)
+        }
+        
+        if y > 0 {
+            var scale = 1.0 + abs(scrollView.contentOffset.y / 500)
+            scale = max(0.0, scale)
+            imageView.transform = CGAffineTransform(scaleX: scale, y: scale)
+        }
+        
+        
+    }
+    
+}
